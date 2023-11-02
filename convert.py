76fccbef8b0c7a648febdd350c03101e12d17944
Cosmos-81 <no-reply@ayase-net.work>
2023-11-02 15:14:28 +0900

    Add error handling and debug messages to image to PDF converter

--- convert.py
+++ convert.py
@@ -3,6 +3,10 @@ import os
 
 def image_to_pdf(input_folder_path, output_folder_path):
 
+    # フォルダ内にjpgファイルが存在しない場合は処理を終了
+    if(len([f for f in os.listdir(input_folder_path) if f.endswith('.jpg')]) == 0):
+        return "フォルダ内にjpgファイルが存在しません。"
+
     # フォルダ内のjpgファイルを取得
     jpg_files = [f for f in os.listdir(input_folder_path) if f.endswith('.jpg')]
 
@@ -20,4 +24,10 @@ def image_to_pdf(input_folder_path, output_folder_path):
         if img.mode == "RGBA":
             img = img.convert("RGB")
         images.append(img)
-    images[0].save(pdf_filename, save_all=True, append_images=images[1:])
+    
+    try:
+        images[0].save(pdf_filename, save_all=True, append_images=images[1:])
+    except Exception as e:
+        return "PDFファイルの作成に失敗しました。"
+    else:
+        return f"保存成功: {pdf_filename}"
--- image_pdf.py
+++ image_pdf.py
@@ -1,3 +1,4 @@
+import os
 import convert
 import tkinter as tk
 from tkinter import filedialog
@@ -13,34 +14,66 @@ def select_output_folder():
     output_folder_entry.insert(0, output_folder_path)
 
 def convert_images_to_pdf():
+
     input_folder_path = input_folder_entry.get()
     output_folder_path = output_folder_entry.get()
-    
+
+    if(input_folder_path == ""):
+        write_debug("入力フォルダを指定してください。")
+        return False
+    if(os.path.exists(input_folder_path) == False):
+        write_debug("入力フォルダが存在しません。")
+        return False
+
+    if(output_folder_path == ""):
+        write_debug("出力フォルダを指定してください。")
+        return False
+    if(os.path.exists(output_folder_path) == False):
+        write_debug("出力フォルダが存在しません。")
+        return False
+
+    write_debug("変換処理を開始します")
+    write_debug(f'入力フォルダ: {input_folder_path}')
+    write_debug(f'出力フォルダ: {output_folder_path}')
+
     # 変換処理を実行
-    convert.image_to_pdf(input_folder_path, output_folder_path)
+    process_status = convert.image_to_pdf(input_folder_path, output_folder_path)
+
+    if(process_status == ""):
+        write_debug("変換処理が完了しました。")
+    else:
+        write_debug(f'E: {process_status}')
+
+
+def write_debug(text):  
+    text_box.insert(tk.END, text + "\n")
+    text_box.see(tk.END)
 
 root = tk.Tk()
 root.title("Image to PDF Converter")
 
-input_folder_label = tk.Label(root, text="Input Folder:")
+input_folder_label = tk.Label(root, text="入力フォルダ:")
 input_folder_label.grid(row=0, column=0, padx=5, pady=5)
 
 input_folder_entry = tk.Entry(root, width=50)
 input_folder_entry.grid(row=0, column=1, padx=5, pady=5)
 
-input_folder_button = tk.Button(root, text="Select Folder", command=select_input_folder)
+input_folder_button = tk.Button(root, text="参照", command=select_input_folder)
 input_folder_button.grid(row=0, column=2, padx=5, pady=5)
 
-output_folder_label = tk.Label(root, text="Output Folder:")
+output_folder_label = tk.Label(root, text="出力フォルダ:")
 output_folder_label.grid(row=1, column=0, padx=5, pady=5)
 
 output_folder_entry = tk.Entry(root, width=50)
 output_folder_entry.grid(row=1, column=1, padx=5, pady=5)
 
-output_folder_button = tk.Button(root, text="Select Folder", command=select_output_folder)
+output_folder_button = tk.Button(root, text="参照", command=select_output_folder)
 output_folder_button.grid(row=1, column=2, padx=5, pady=5)
 
-submit_button = tk.Button(root, text="Convert to PDF", command=convert_images_to_pdf)
+submit_button = tk.Button(root, text="変換開始", command=convert_images_to_pdf)
 submit_button.grid(row=2, column=1, padx=5, pady=5)
+
+text_box = tk.Text(root, height=15)
+text_box.grid(row=3, column=0, columnspan=3, padx=5, pady=5)
 
 root.mainloop()
